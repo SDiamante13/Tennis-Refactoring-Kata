0eaaff51c9e43b1fc0c528be800cb45c5ec095e3
Steven Diamante <stevendiamante13@gmail.com>
2022-05-09 22:37:24 -0400

    Upgrade to JUnit5. Delete tests for screencast.

---
+++ java/src/main/java/tennis/TennisGame1.java
@@ -0,0 +1,80 @@
+package tennis;
+
+public class TennisGame1 implements TennisGame {
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame1(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName.equals("player1")) {
+            player1Score += 1;
+        } else {
+            player2Score += 1;
+        }
+    }
+
+    public String getScore() {
+        StringBuilder score = new StringBuilder();
+        int tempScore = 0;
+        if (player1Score == player2Score) {
+            switch (player1Score) {
+                case 0:
+                    score.append("Love-All");
+                    break;
+                case 1:
+                    score.append("Fifteen-All");
+                    break;
+                case 2:
+                    score.append("Thirty-All");
+                    break;
+                default:
+                    score.append("tennis.Deuce");
+                    break;
+
+            }
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) {
+                score.append("Advantage player1");
+            } else if (minusResult == -1) {
+                score.append("Advantage player2");
+            } else if (minusResult >= 2) {
+                score.append("Win for player1");
+            } else {
+                score.append("Win for player2");
+            }
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) {
+                    tempScore = player1Score;
+                } else {
+                    score.append("-");
+                    tempScore = player2Score;
+                }
+
+                switch (tempScore) {
+                    case 0:
+                        score.append("Love");
+                        break;
+                    case 1:
+                        score.append("Fifteen");
+                        break;
+                    case 2:
+                        score.append("Thirty");
+                        break;
+                    case 3:
+                        score.append("Forty");
+                        break;
+                }
+            }
+        }
+        return score.toString();
+    }
+}
---
+++ java/src/test/java/tennis/TennisTest.java
@@ -0,0 +1,13 @@
+package tennis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TennisTest {
+
+    @Test
+    void name() {
+        
+    }
+}
