fbba5a45af072e358c232a1599501362aa83fe4a
Steven Diamante <stevendiamante13@gmail.com>
2022-06-09 15:16:32 -0400

    Refactor further. Moving more logic into the Player class.

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -5,105 +5,133 @@ public class TennisGame1 implements TennisGame {
     private static final int ONE_POINT = 1;
     private static final int TWO_POINTS = 2;
     private static final int THREE_POINTS = 3;
-    private static final int ADVANTAGE_THRESHOLD = 4;
 
     // It passes all the tests
     // It minimizes duplication in all its forms
     // It expresses its intent clearly to the reader
     // It removes unnecessary elements
 
-    // Step 0: Identify Code smells - Long method, Heavy Indentation, Temporary Field
-    // Step 1: Rename methods, fields, and variables to understand the domain better.
-    // Step 2: Extract methods to paint a clear picture of what the code is doing.
-    // Step 3: Remove temporary variables. Return fields early.
-    // Step 4: Substitute Algorithm for Advantage method
-    // Step 5: Substitute Algorithm for ScoreBothPlayers method
     // Step 6: Eliminate Primitive Obsession by introduction a Value Object called Player to hold the score of the player.
-    
-    private int playerOneScore = ZERO_POINTS;
-    private int playerTwoScore = ZERO_POINTS;
-    private String player1Name;
-    private String player2Name;
+    // Step 7: Inline methods to further reduce duplication and complexity
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    private Player player1;
+    private Player player2;
+
+    public TennisGame1(String player1Name, String player2Name) { // can migrate constructor to take in two players instead of names.
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
     }
 
-    public void wonPoint(String playerName) {
-        // replace with ternary?
-        if (playerName == "player1")
-            playerOneScore += ONE_POINT;
-        else
-            playerTwoScore += ONE_POINT;
+    public void wonPoint(String playerName) { // this can be replaced by just add points to players. This would require an interface change.
+        if ("player1".equals(playerName)) {
+            player1.addPoint();
+        } else {
+            player2.addPoint();
+        }
     }
 
     public String getScore() {
-        if (playerOneScore == playerTwoScore) {
-            return tiedScore();
+        if (player1.isTiedWith(player2.score())) {
+            return Points.ofTied(player1.score()).displayName();
         }
-        boolean playersAreInAdvantagePlay = playerOneScore >= ADVANTAGE_THRESHOLD || playerTwoScore >= ADVANTAGE_THRESHOLD;
-        if (playersAreInAdvantagePlay) {
-            return scoreForAdvantagePlay();
-        } else {
-            return scoreForBothPlayers();
+
+        if (player1.hasAdvantage(player2.score()) && player1.isAheadByOnePoint(player2.score())) {
+                return "Advantage player1"; // can add ofAdvantage creation method to Points enum
         }
-    }
 
+        if (player1.hasAdvantage(player2.score()) && player1.hasWon(player2.score())) {
+            return "Win for player1";
+        }
 
-    private String scoreForBothPlayers() {
-        return String.format("%s-%s", translate(playerOneScore), translate(playerTwoScore));
+        if (player2.hasAdvantage(player1.score()) && player2.isAheadByOnePoint(player1.score())) {
+                return "Advantage player2";
+        }
+
+        if (player2.hasAdvantage(player1.score()) && player2.hasWon(player1.score())) {
+            return "Win for player2";
+        }
+
+        return String.format("%s-%s", translate(player1.score()), translate(player2.score()));
     }
 
     private String translate(int score) {
-        // make an enum called Score (ZERO, ONE, TWO, THREE, ) with mappings to String values
-        switch (score) {
-            case ONE_POINT:
-                return "Fifteen";
-            case TWO_POINTS:
-                return "Thirty";
-            case THREE_POINTS:
-                return "Forty";
-            default:
-                return "Love";
+        return Points.of(score).displayName();
+    }
+
+    enum Points {
+        ZERO("Love"), ONE("Fifteen"), TWO("Thirty"), THREE("Forty"), ZERO_ALL("Love-All"), ONE_ALL("Fifteen-All"), TWO_ALL("Thirty-All"), DEUCE("Deuce");
+
+        private final String displayName;
+
+        Points(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String displayName() {
+            return displayName;
+        }
+
+        public static Points of(int score) {
+            switch (score) {
+                case ONE_POINT:
+                    return Points.ONE;
+                case TWO_POINTS:
+                    return Points.TWO;
+                case THREE_POINTS:
+                    return Points.THREE;
+                default:
+                    return Points.ZERO;
+
+            }
+        }
+
+        public static Points ofTied(int score) {
+            switch (score) {
+                case ZERO_POINTS:
+                    return Points.ZERO_ALL;
+                case ONE_POINT:
+                    return Points.ONE_ALL;
+                case TWO_POINTS:
+                    return Points.TWO_ALL;
+                default:
+                    return Points.DEUCE;
+            }
         }
     }
 
-    private String scoreForAdvantagePlay() {
-        if (playerOneIsAheadByOnePoint()) { // this could be  Enum called ScoringState {ADVANTAGE_PLAYER_ONE, ADVANTAGE_PLAYER_TWO, PLAYER_ONE_WIN, PLAYER_TWO_WIN}
-            return "Advantage player1";
+    static class Player {
+        private static final int ADVANTAGE_THRESHOLD = 4;
+
+        private final String name;
+        private int score;
+
+        Player(String name) {
+            this.name = name;
+            this.score = ZERO_POINTS;
         }
-        if (playerTwoIsAheadByOnePoint()) {
-            return "Advantage player2";
+
+        public void addPoint() {
+            this.score++;
         }
-        if (playerOneHasWon()) {
-            return "Win for player1";
+
+        public int score() {
+            return score;
         }
-        return "Win for player2";
-    }
 
-    private boolean playerOneHasWon() {
-        return playerOneScore - playerTwoScore >= TWO_POINTS;
-    }
+        private boolean hasAdvantage(int otherScore) {
+            return score >= ADVANTAGE_THRESHOLD && score > otherScore;
+        }
 
-    private boolean playerTwoIsAheadByOnePoint() {
-        return playerTwoScore - playerOneScore == ONE_POINT;
-    }
+        public boolean isTiedWith(int otherScore) {
+            return score == otherScore;
+        }
 
-    private boolean playerOneIsAheadByOnePoint() {
-        return playerOneScore - playerTwoScore == ONE_POINT;
-    }
+        private boolean hasWon(int otherScore) {
+            return score - otherScore >= TWO_POINTS;
+        }
 
-    private String tiedScore() {
-        switch (playerOneScore) {
-            case ZERO_POINTS:
-                return "Love-All";
-            case ONE_POINT:
-                return "Fifteen-All";
-            case TWO_POINTS:
-                return "Thirty-All";
-            default:
-                return "Deuce";
+        private boolean isAheadByOnePoint(int otherScore) {
+            return score - otherScore == ONE_POINT;
         }
     }
 }
