a6517311fea2b5b7fc967ef6616639a85c5ac4f7
Steven Diamante <stevendiamante13@gmail.com>
2022-06-17 15:11:35 -0400

    Refactoring presentation for teams at work.

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -2,76 +2,142 @@ package tennis;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGame1(String player1Name, String player2Name) {
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals("player1")) {
+            player1.addPoint();
+        } else {
+            player2.addPoint();
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+        return Arbiter.determineResult(player1, player2).getScoreAsText();
+    }
 
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+}
+
+class Player {
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    boolean isTiedWith(int otherScore) {
+        return score == otherScore;
+    }
+
+    public int score() {
         return score;
     }
+
+    public void addPoint() {
+        score++;
+    }
 }
+
+class Arbiter {
+    public static Result determineResult(Player player1, Player player2) {
+        if (player1.isTiedWith(player2.score())) {
+            return new DrawResult(player1.score(), player2.score());
+        } else if (player1.score() >= 4 || player2.score() >= 4) {
+            return new AdvantageOrWinResult(player1.score(), player2.score());
+        } else {
+            return new OngoingResult(player1.score(), player2.score());
+        }
+    }
+}
+
+abstract class Result {
+    int player1Score;
+    int player2Score;
+
+    Result(int player1Score, int player2Score) {
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+    }
+
+    abstract String getScoreAsText();
+}
+
+class DrawResult extends Result {
+
+    public DrawResult(int player1Score, int player2Score) {
+        super(player1Score, player2Score);
+    }
+
+    @Override
+    String getScoreAsText() {
+        switch (player1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
+    }
+}
+
+class AdvantageOrWinResult extends Result {
+
+    AdvantageOrWinResult(int player1Score, int player2Score) {
+        super(player1Score, player2Score);
+    }
+
+    @Override
+    String getScoreAsText() {
+        int minusResult = player1Score - player2Score;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+}
+
+class OngoingResult extends Result {
+
+    OngoingResult(int player1Score, int player2Score) {
+        super(player1Score, player2Score);
+    }
+
+    @Override
+    String getScoreAsText() {
+        return getScoreText(player1Score) + "-" + getScoreText(player2Score);
+    }
+
+    private String getScoreText(int tempScore) {
+        switch (tempScore) {
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "Love";
+        }
+    }
+}
+
