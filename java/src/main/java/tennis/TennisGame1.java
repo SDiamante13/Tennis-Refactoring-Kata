ee883518e6f596db80b95abbc3bd2828c38a9163
Steven Diamante <stevendiamante13@gmail.com>
2022-06-29 23:00:46 -0400

    Low Hanging Fruit. Unroll For Loop. Replace Conditional with Polymorphism.

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -1,9 +1,13 @@
 package tennis;
 
+// Low-Hanging Fruit (rename field, rename method, extract method, remove temp variables)
+// Unroll For Loop
+// Replace Conditional with Polymorphism
+
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -14,64 +18,103 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            player1Score += 1;
         else
-            m_score2 += 1;
+            player2Score += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+        return Arbiter.determineResult(player1Score, player2Score).getScoreAsText();
+    }
+}
 
-            }
+abstract class Result {
+    int player1Score;
+    int player2Score;
+
+    public Result(int player1Score, int player2Score) {
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+    }
+
+    abstract String getScoreAsText();
+}
+
+class DrawResult extends Result {
+
+    public DrawResult(int player1Score, int player2Score) {
+        super(player1Score, player2Score);
+    }
+
+    @Override
+    String getScoreAsText() {
+        switch (player1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+}
+
+class AdvantageOrWinResult extends Result {
+
+    public AdvantageOrWinResult(int player1Score, int player2Score) {
+        super(player1Score, player2Score);
+    }
+
+    @Override
+    String getScoreAsText() {
+        int minusResult = player1Score - player2Score;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+
+    }
+}
+
+class OngoingResult extends Result {
+    public OngoingResult(int player1Score, int player2Score) {
+        super(player1Score, player2Score);
+    }
+
+    @Override
+    String getScoreAsText() {
+        return getScoreAsString(player1Score) + "-" + getScoreAsString(player2Score);
+    }
+
+    private String getScoreAsString(int score) {
+        switch (score) {
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "Love";
         }
-        return score;
+    }
+}
+
+class Arbiter {
+    public static Result determineResult(int player1Score, int player2Score) {
+        if (player1Score == player2Score) {
+            return new DrawResult(player1Score, player2Score);
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            return new AdvantageOrWinResult(player1Score, player2Score);
+        } else {
+            return new OngoingResult(player1Score, player2Score);
+        }
     }
 }
